6c76ce4ec3ffd6098a5b6345bf8dc793810152e4
Your Name <marlonyurimi@gmail.com>
2022-09-26 10:27:19 -0300

    fix function

--- src/Pages/DogPage.jsx
+++ src/Pages/DogPage.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 
 export default function DogPage() {
     let LApi = "https://dog.ceo/api/breeds/image/random";
 
-    function GetDogs() {
-        axios.get(LApi).then(() => {
-            console.log(response)
-        }
+    function getApi(){
+        axios.get(LApi).then(res =>{
+            console.log(res)
+        })
     }
+
+    useEffect(()=>{
+        getApi()
+    })
 
     return (
         <>
